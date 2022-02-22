7c54546e6c96aa8b6a5a0b508f6c997114b3ca8f
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-22 21:37:26 +0330

    header static styles completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,40 @@
 import style from "./Header.module.css";
-import { BiArrowBack, BiCart } from "react-icons/bi";
+import {
+  BiCart,
+  BiDownArrowAlt,
+  BiMenu,
+  BiUpArrowAlt,
+  BiX,
+} from "react-icons/bi";
+import { useState } from "react";
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const initial = {
+    digital: false,
+    clothes: false,
+    toys: false,
+    health: false,
+    book: false,
+    car: false,
+  };
+  const [showSub, setShowSub] = useState(initial);
+
+  const showMenuHandler = () => setShowMenu(true);
+  const closeMenuHandler = () => setShowMenu(false);
+  const showSubHandler = (property) =>
+    setShowSub({
+      ...initial,
+      [property]: !showSub[property],
+    });
+
+  const arrow = () => {
+    return showSub.digital ? (
+      <BiUpArrowAlt className={style.hidden} />
+    ) : (
+      <BiDownArrowAlt className={style.hidden} />
+    );
+  };
+
   return (
     <header className={style.mainHeader}>
       <div className="container">
@@ -11,15 +45,34 @@ const Header = () => {
               <input type="text" placeholder="جستجو ..."></input>
             </div>
           </div>
-          <div className={style.topHeaders}>
-            <button className={style.btn}>ورود | ثبت نام</button>
-            <BiCart className={style.icon} />
+          <div className={style.bottomHeader}>
+            <BiMenu className={style.menuBtn} onClick={showMenuHandler} />
+            <div className={style.topHeaders}>
+              <button className={style.btn}>ورود | ثبت نام</button>
+              <BiCart className={style.icon} />
+            </div>
           </div>
         </div>
-        <ul className={style.headerMenu}>
+
+        <ul
+          className={`${style.headerMenu} ${
+            showMenu ? style.showMobileMenu : ""
+          }`}
+        >
+          <BiX className={style.closeBtn} onClick={closeMenuHandler} />
           <li className={style.menuItem}>
-            کالای دیجیتال
-            <ul className={style.subMenu}>
+            <span
+              className={showSub.digital ? style.active : ""}
+              onClick={() => showSubHandler("digital")}
+            >
+              کالای دیجیتال
+              {arrow()}
+            </span>
+            <ul
+              className={`${style.hide} ${
+                showSub.digital ? style.subMenu : ""
+              }`}
+            >
               <li className={style.subMenuItem}>گوشی موبایل</li>
               <li className={style.subMenuItem}>لپتاپ</li>
               <li className={style.subMenuItem}>تبلت</li>
@@ -28,16 +81,34 @@ const Header = () => {
             </ul>
           </li>
           <li className={style.menuItem}>
-            مد و پوشاک
-            <ul className={style.subMenu}>
+            <span
+              className={showSub.clothes ? style.active : ""}
+              onClick={() => showSubHandler("clothes")}
+            >
+              مد و پوشاک
+              {arrow()}
+            </span>
+            <ul
+              className={`${style.hide} ${
+                showSub.clothes ? style.subMenu : ""
+              }`}
+            >
               <li className={style.subMenuItem}>مردانه</li>
               <li className={style.subMenuItem}>زنانه</li>
               <li className={style.subMenuItem}>بچه گانه</li>
             </ul>
           </li>
           <li className={style.menuItem}>
-            اسباب بازی
-            <ul className={style.subMenu}>
+            <span
+              className={showSub.toys ? style.active : ""}
+              onClick={() => showSubHandler("toys")}
+            >
+              اسباب بازی
+              {arrow()}
+            </span>
+            <ul
+              className={`${style.hide} ${showSub.toys ? style.subMenu : ""}`}
+            >
               <li className={style.subMenuItem}>فکری آموزشی</li>
               <li className={style.subMenuItem}>لگو و ساختنی</li>
               <li className={style.subMenuItem}>عروسک و فیگور</li>
@@ -46,32 +117,47 @@ const Header = () => {
             </ul>
           </li>
           <li className={style.menuItem}>
-            زیبایی و سلامت
-            <ul className={style.subMenu}>
+            <span
+              className={showSub.health ? style.active : ""}
+              onClick={() => showSubHandler("health")}
+            >
+              زیبایی و سلامت
+              {arrow()}
+            </span>
+            <ul
+              className={`${style.hide} ${showSub.health ? style.subMenu : ""}`}
+            >
               <li className={style.subMenuItem}>لوازم آرایشی</li>
               <li className={style.subMenuItem}>لوازم بهداشتی</li>
               <li className={style.subMenuItem}>طلا و زیورآلات زنانه</li>
             </ul>
           </li>
           <li className={style.menuItem}>
-            کتاب
-            <ul className={style.subMenu}>
+            <span
+              className={showSub.book ? style.active : ""}
+              onClick={() => showSubHandler("book")}
+            >
+              کتاب
+              {arrow()}
+            </span>
+            <ul
+              className={`${style.hide} ${showSub.book ? style.subMenu : ""}`}
+            >
               <li className={style.subMenuItem}>کتاب چاپی</li>
               <li className={style.subMenuItem}>کتاب صوتی</li>
               <li className={style.subMenuItem}>مجلات داخلی و خارجی</li>
             </ul>
           </li>
           <li className={style.menuItem}>
-            لوازم خودرو
-            <ul className={style.subMenu}>
-              <li className={style.subMenuItem}>
-                لوازم جانبی
-                <ul className={style.subMenu2}>
-                  <li className={style.subMenuItem}>کتاب چاپی</li>
-                  <li className={style.subMenuItem}>کتاب صوتی</li>
-                  <li className={style.subMenuItem}>مجلات داخلی و خارجی</li>
-                </ul>
-              </li>
+            <span
+              className={showSub.car ? style.active : ""}
+              onClick={() => showSubHandler("car")}
+            >
+              لوازم خودرو
+              {arrow()}
+            </span>
+            <ul className={`${style.hide} ${showSub.car ? style.subMenu : ""}`}>
+              <li className={style.subMenuItem}>لوازم جانبی</li>
               <li className={style.subMenuItem}>لوازم یدکی</li>
               <li className={style.subMenuItem}>لوازم مصرفی</li>
             </ul>
