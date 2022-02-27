ddd21f5c96122b0c763363922ed84c43fd367500
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-27 22:33:19 +0330

    Error component created

---
+++ src/components/ErrorBox/ErrorBox.js
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import { getDataApi } from "../../redux/productsSlice";
+import style from "./Error.module.css";
+const ErrorBox = ({ error }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className={style.errorBox}>
+      <div className="container">
+        <p>
+          {error}
+          <button onClick={() => dispatch(getDataApi())}>تلاش مجدد</button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorBox;
