a9253bd54f27c350706cff274180e951a5f21cd6
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-27 23:23:43 +0330

    use seprate function

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,22 +1,10 @@
 import style from "./Products.module.css";
 import { BiFilter } from "react-icons/bi";
-import { useEffect } from "react";
-import { getDataApi } from "../../redux/productsSlice";
-import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../components/Loading/Loading";
 import ErrorBox from "../../components/ErrorBox/ErrorBox";
 import { useNavigate, useParams } from "react-router-dom";
-
-const separatePrice = (number) => {
-  number += "";
-  number = number.replace(",", "");
-  let x = number.split(".");
-  let y = x[0];
-  let z = x.length > 1 ? "." + x[1] : "";
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(y)) y = y.replace(rgx, "$1" + "٫" + "$2");
-  return y + z;
-};
+import sepratePrice from "../../utils/sepratePrice";
+import { useSelector } from "react-redux";
 
 const Products = () => {
   const { group } = useParams();
@@ -64,8 +52,8 @@ const Product = ({ product }) => {
       </div>
       <div className={style.description}>
         <span className={style.productName}>{product.name}</span>
-        <span className={style.availabel}>موجود در انبار</span>
-        <span className={style.price}>{separatePrice(product.price)} تومن</span>
+        <span className={style.available}>موجود در انبار</span>
+        <span className={style.price}>{sepratePrice(product.price)} تومن</span>
         <button className={style.btn}>مشاهده جزئیات</button>
       </div>
     </div>
