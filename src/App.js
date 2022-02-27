c4ded23ce2b44f84b4682072a4e0a7155ae8ff59
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-27 22:27:13 +0330

    add some routes for products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useEffect } from "react";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Products from "./pages/Products/Products";
+import Layout from "./Layout/Layout";
+import HomePage from "./pages/HomePage";
+import Product from "./pages/Product/Product";
+import { getDataApi } from "./redux/productsSlice";
 
 function App() {
-  return <Header />;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDataApi());
+  }, []);
+  
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products">
+          <Route path="/products/:group" element={<Products />} />
+          <Route path="/products/:group/:productId" element={<Product />} />
+        </Route>
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
