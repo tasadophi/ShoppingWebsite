02fdd78467a872fd285a51bd28cd1005953cade4
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-03-03 00:47:32 +0330

    filterProducts added

--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -15,22 +15,66 @@ export const getDataApi = createAsyncThunk(
 
 let initialState = {
   error: null,
-  products: {},
+  products: [],
+  allProducts: [],
   loading: false,
+  cart: [],
+  filters: [],
 };
 
 const productSlice = createSlice({
   name: "products",
   initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    deleteFilters: (state) => {
+      state.filters = [];
+      state.products = state.allProducts;
+    },
+    filterProducts: (state, action) => {
+      const payload = action.payload;
+      if (payload.checked) state.filters.push(payload);
+      else
+        state.filters = state.filters.filter(
+          (filter) => filter.id !== payload.id
+        );
+      const filteredProducts = state.allProducts.filter(
+        (product) => product.category === payload.category
+      );
+      const products = [];
+      filteredProducts.forEach((p) => {
+        const specifications = p.specifications;
+        state.filters.forEach((filter) => {
+          specifications.forEach((s) => {
+            if (s.filter === filter.type && s.value === filter.value) {
+              products.push(p);
+            }
+          });
+        });
+      });
+      products.length
+        ? (state.products = [...new Set(products)])
+        : (state.products = state.allProducts);
+    },
+  },
   extraReducers: {
     [getDataApi.pending]: (state) => {
-      return { ...state, error: null, products: {}, loading: true };
+      return {
+        ...state,
+        error: null,
+        products: [],
+        allProducts: [],
+        loading: true,
+      };
     },
     [getDataApi.fulfilled]: (state, action) => {
       return {
         ...state,
         error: null,
         products: action.payload,
+        allProducts: action.payload,
         loading: false,
       };
     },
@@ -38,11 +82,13 @@ const productSlice = createSlice({
       return {
         ...state,
         error: action.payload.message,
-        products: {},
+        products: [],
+        allProducts: [],
         loading: false,
       };
     },
   },
 });
-export const { initData } = productSlice.actions;
+export const { addProduct, filterProducts, deleteFilters } =
+  productSlice.actions;
 export default productSlice.reducer;
