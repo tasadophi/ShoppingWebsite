b5cd5ac6d3e2c87d201c5ac41ca2ab19e737df14
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-03-17 19:31:40 +0330

    footer created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import style from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={style.mainFooter}>
+      <div className="container">ساخته شده توسط علی محمودی</div>
+    </footer>
+  );
+};
+
+export default Footer;
